56c652c0708b1aabe778bb487b51d3a8428d6d6c
Gabriel Ames-Bull <g.ames-bull@tangente.co>
2013-03-13 15:06:53 -0400

    Added new tests

--- test/UPS/Tests/TrackingTest.php
+++ test/UPS/Tests/TrackingTest.php
@@ -12,14 +12,20 @@ use UPS;
 class TrackingTest extends \PHPUnit_Framework_TestCase {	
 	// Test track
 	public function testTrack() {
-		$tracking = new UPS\Tracking($GLOBALS['UPS_ACCESS_KEY'], $GLOBALS['UPS_USER_ID'], $GLOBALS['UPS_PASSWORD'], true);
-		$tracking->setContext('unit test');
+		$tracking = new UPS\Tracking($GLOBALS['UPS_ACCESS_KEY'], $GLOBALS['UPS_USER_ID'], $GLOBALS['UPS_PASSWORD']);
 
 		$shipment = $tracking->track('1Z12345E1512345676');
 
-		// Test tracking
+		// Test response
 		$this->assertInstanceOf('stdClass', $shipment);
 		$this->assertObjectHasAttribute('Package', $shipment);
+	}
+
+	// Test track with context
+	public function testTrackContext() {
+		$tracking = new UPS\Tracking($GLOBALS['UPS_ACCESS_KEY'], $GLOBALS['UPS_USER_ID'], $GLOBALS['UPS_PASSWORD']);
+		$tracking->setContext('unit test');
+		$tracking->track('1Z12345E1512345676');
 
 		// Test context
 		$this->assertRegExp('{<CustomerContext>.?unit test.?</CustomerContext>}msU', $tracking->response);
