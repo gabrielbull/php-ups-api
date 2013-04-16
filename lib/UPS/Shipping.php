d619188e543f5837d3a4c31b22d0abde64da9480
Jason Baronciani <jasonbar@gmail.com>
2013-04-15 23:33:01 -0400

    Initial support for generating labels

--- lib/UPS/Shipping.php
+++ lib/UPS/Shipping.php
@@ -15,6 +15,9 @@ use DOMDocument,
  * @package ups
  */
 class Shipping extends UPS {
+	const REQ_VALIDATE = 'validate';
+	const REQ_NONVALIDATE = 'nonvalidate';
+
 	private $shipConfirmEndpoint = '/ShipConfirm';
 	private $shipAcceptEndpoint = '/ShipAccept';
 	private $voidEndpoint = '/Void';
@@ -23,16 +26,22 @@ class Shipping extends UPS {
 	/**
 	 * Create a Shipment Confirm request (generate a digest)
 	 *
-	 * @param  
+	 * @param  string $validation A UPS_Shipping::REQ_* constant (or null)
+	 * @param  stdClass $shipment Shipment data container.
+	 * @param  stdClass $labelSpecOpts LabelSpecification data. Optional
+	 * @param  stdClass $receiptSpecOpts ReceiptSpecification data. Optional
 	 * @return stdClass
 	 */
-	public function confirm() {
-		$request = $this->createConfirmRequest();
+	public function confirm($validation, $shipment, $labelSpecOpts = null, $receiptSpecOpts = null) {
+		$request = $this->createConfirmRequest($validation, $shipment, $labelSpecOpts, $receiptSpecOpts);
+
+//		return $request;
+
 		$response = $this->request($this->createAccess(), $request, $this->compileEndpointUrl($this->shipConfirmEndpoint));
 
 		if ($response->Response->ResponseStatusCode == 0) {
 			throw new Exception(
-				"Failure ({$response->Response->Error->ErrorSeverity}): {$response->Response->Error->ErrorDescription}", 
+				"Failure ({$response->Response->Error->ErrorCode}: {$response->Response->Error->ErrorSeverity}): {$response->Response->Error->ErrorDescription}", 
 				(int) $response->Response->Error->ErrorCode
 			);
 		} else {
@@ -47,17 +56,324 @@ class Shipping extends UPS {
 	 * @see UPS\Shipping->confirm for parameters
 	 * @return string
 	 */
-	private function createConfirmRequest() {
+	private function createConfirmRequest($validation, $shipment, $labelSpecOpts, $receiptSpecOpts) {
 		$xml = new DOMDocument();
 		$xml->formatOutput = true;
 
+		// Page 45
 		$container = $xml->appendChild($xml->createElement('ShipmentConfirmRequest'));
+
+		// Page 45
 		$request = $container->appendChild($xml->createElement('Request'));
 
 		$node = $xml->importNode($this->createTransactionNode(), true);
 		$request->appendChild($node);
 
 		$request->appendChild($xml->createElement('RequestAction', 'ShipConfirm'));
+		$request->appendChild($xml->createElement('RequestOption', $validation ?: ''));
+
+		// Page 47
+		$shipmentNode = $container->appendChild($xml->createElement('Shipment'));
+
+		if ($shipment->Description) {
+			$shipmentNode->appendChild($xml->createElement('Description', $shipment->Description));
+		}
+
+		if ($shipment->ReturnService) {
+			$node = $shipmentNode->appendChild($xml->createElement('ReturnService'));
+
+			$node->appendChild($xml->createElement('Code', $shipment->ReturnService->Code));
+
+			if ($shipment->ReturnService->DocumentsOnly) {
+				$node->appendChild($xml->createElement('DocumentsOnly'));
+			}
+		}
+
+		$shipperNode = $shipmentNode->appendChild($xml->createElement('Shipper'));
+
+		$shipperNode->appendChild($xml->createElement('Name', $shipment->Shipper->Name));
+
+		if ($shipment->Shipper->AttentionName) {
+			$shipperNode->appendChild($xml->createElement('AttentionName', $shipment->Shipper->AttentionName));
+		}
+
+		if ($shipment->Shipper->CompanyDisplayableName) {
+			$shipperNode->appendChild($xml->createElement('CompanyDisplayableName', $shipment->Shipper->CompanyDisplayableName));
+		}
+
+		$shipperNode->appendChild($xml->createElement('ShipperNumber', $shipment->Shipper->ShipperNumber));
+
+		if ($shipment->Shipper->TaxIdentificationNumber) {
+			$shipperNode->appendChild($xml->createElement('TaxIdentificationNumber', $shipment->Shipper->TaxIdentificationNumber));
+		}
+
+		if ($shipment->Shipper->PhoneNumber) {
+			$shipperNode->appendChild($xml->createElement('PhoneNumber', $shipment->Shipper->PhoneNumber));
+		}
+
+		if ($shipment->Shipper->FaxNumber) {
+			$shipperNode->appendChild($xml->createElement('FaxNumber', $shipment->Shipper->FaxNumber));
+		}
+
+		if ($shipment->Shipper->EMailAddress) {
+			$shipperNode->appendChild($xml->createElement('EMailAddress', $shipment->Shipper->EMailAddress));
+		}
+
+		$addressNode = $xml->importNode($this->compileAddressNode($shipment->Shipper->Address), true);
+		$shipperNode->appendChild($addressNode);
+
+		$shipToNode = $shipmentNode->appendChild($xml->createElement('ShipTo'));
+
+		$shipToNode->appendChild($xml->createElement('CompanyName', $shipment->ShipTo->CompanyName));
+
+		if ($shipment->ShipTo->AttentionName) {
+			$shipToNode->appendChild($xml->createElement('AttentionName', $shipment->ShipTo->AttentionName));
+		}
+
+		if ($shipment->ShipTo->PhoneNumber) {
+			$shipToNode->appendChild($xml->createElement('PhoneNumber', $shipment->ShipTo->PhoneNumber));
+		}
+
+		if ($shipment->ShipTo->FaxNumber) {
+			$shipToNode->appendChild($xml->createElement('FaxNumber', $shipment->ShipTo->FaxNumber));
+		}
+
+		if ($shipment->ShipTo->EMailAddress) {
+			$shipToNode->appendChild($xml->createElement('EMailAddress', $shipment->ShipTo->EMailAddress));
+		}
+
+		$addressNode = $xml->importNode($this->compileAddressNode($shipment->ShipTo->Address), true);
+
+		if ($shipment->ShipTo->ResidentialAddress) {
+			$addressNode->appendChild($xml->createElement('ResidentialAddress'));
+		}
+
+		if ($shipment->ShipTo->LocationID) {
+			$addressNode->appendChild($xml->createElement('LocationID', strtoupper($shipment->ShipTo->LocationID)));
+		}
+
+		$shipToNode->appendChild($addressNode);
+
+		if ($shipment->ShipFrom) {
+			$shipFromNode = $shipmentNode->appendChild($xml->createElement('ShipFrom'));
+
+			$shipFromNode->appendChild($xml->createElement('CompanyName', $shipment->ShipFrom->CompanyName));
+
+			if ($shipment->ShipFrom->AttentionName) {
+				$shipFromNode->appendChild($xml->createElement('AttentionName', $shipment->ShipFrom->AttentionName));
+			}
+
+			if ($shipment->ShipFrom->PhoneNumber) {
+				$shipFromNode->appendChild($xml->createElement('PhoneNumber', $shipment->ShipFrom->PhoneNumber));
+			}
+
+			if ($shipment->ShipFrom->FaxNumber) {
+				$shipFromNode->appendChild($xml->createElement('FaxNumber', $shipment->ShipFrom->FaxNumber));
+			}
+
+			$addressNode = $xml->importNode($this->compileAddressNode($shipment->ShipFrom->Address), true);
+			$shipFromNode->appendChild($addressNode);
+		}
+
+		if ($shipment->SoldTo) {
+			$soldToNode = $shipmentNode->appendChild($xml->createElement('SoldTo'));
+
+			if ($shipment->SoldTo->Option) {
+				$soldToNode->appendChild($xml->createElement('Option', $shipment->SoldTo->Option));
+			}
+
+			$soldToNode->appendChild($xml->createElement('CompanyName', $shipment->SoldTo->CompanyName));
+
+			if ($shipment->SoldTo->AttentionName) {
+				$soldToNode->appendChild($xml->createElement('AttentionName', $shipment->SoldTo->AttentionName));
+			}
+
+			if ($shipment->SoldTo->PhoneNumber) {
+				$soldToNode->appendChild($xml->createElement('PhoneNumber', $shipment->SoldTo->PhoneNumber));
+			}
+
+			if ($shipment->SoldTo->FaxNumber) {
+				$soldToNode->appendChild($xml->createElement('FaxNumber', $shipment->SoldTo->FaxNumber));
+			}
+
+			if ($shipment->SoldTo->Address) {
+				$addressNode = $xml->importNode($this->compileAddressNode($shipment->SoldTo->Address), true);
+				$soldToNode->appendChild($addressNode);
+			}
+		}
+
+		if ($shipment->PaymentInformation) {
+			$paymentNode = $shipmentNode->appendChild($xml->createElement('PaymentInformation'));
+
+			if ($shipment->PaymentInformation->Prepaid) {
+				$node = $paymentNode->appendChild($xml->createElement('Prepaid'));
+				$node = $node->appendChild($xml->createElement('BillShipper'));
+
+				if ($shipment->PaymentInformation->Prepaid->BillShipper->AccountNumber) {
+					$node->appendChild($xml->createElement('AccountNumber', $shipment->PaymentInformation->Prepaid->BillShipper->AccountNumber));
+				} else if ($shipment->PaymentInformation->Prepaid->BillShipper->CreditCard) {
+					$ccNode = $node->appendChild($xml->createElement('CreditCard'));
+					$ccNode->appendChild($xml->createElement('Type', $shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->Type));
+					$ccNode->appendChild($xml->createElement('Number', $shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->Number));
+					$ccNode->appendChild($xml->createElement('ExpirationDate', $shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->ExpirationDate));
+
+					if ($shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->SecurityCode) {
+						$ccNode->appendChild($xml->createElement('SecurityCode', $shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->SecurityCode));
+					}
+
+					if ($shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->Address) {
+						$addressNode = $xml->importNode($this->compileAddressNode($shipment->PaymentInformation->Prepaid->BillShipper->CreditCard->Address), true);
+						$ccNode->appendChild($addressNode);
+					}
+				}
+			} else if ($shipment->PaymentInformation->BillThirdParty) {
+				$node = $paymentNode->appendChild($xml->createElement('BillThirdParty'));
+				$btpNode = $node->appendChild($xml->createElement('BillThirdPartyShipper'));
+				$btpNode->appendChild($xml->createElement('AccountNumber', $shipment->PaymentInformation->BillThirdParty->AccountNumber));
+
+				$tpNode = $btpNode->appendChild($xml->createElement('ThirdParty'));
+				$addressNode = $tpNode->appendChild($xml->createElement('Address'));
+
+				if ($shipment->PaymentInformation->BillThirdParty->ThirdParty->Address->PostalCode) {
+					$addressNode->appendChild($xml->createElement('PostalCode', $shipment->PaymentInformation->BillThirdParty->ThirdParty->Address->PostalCode));
+				}
+
+				$addressNode->appendChild($xml->createElement('CountryCode', $shipment->PaymentInformation->BillThirdParty->ThirdParty->Address->CountryCode));
+			} else if ($shipment->PaymentInformation->FreightCollect) {
+				$node = $paymentNode->appendChild($xml->createElement('FreightCollect'));
+				$brNode = $node->appendChild($xml->createElement('BillReceiver'));
+				$brNode->appendChild($xml->createElement('AccountNumber', $shipment->PaymentInformation->FreightCollect->BillReceiver->AccountNumber));
+
+				if ($shipment->PaymentInformation->FreightCollect->BillReceiver->Address) {
+					$addressNode = $brNode->appendChild($xml->createElement('Address'));
+					$addressNode->appendChild($xml->createElement('PostalCode', $shipment->PaymentInformation->FreightCollect->BillReceiver->Address->PostalCode));
+				}
+			} else if ($shipment->PaymentInformation->ConsigneeBilled) {
+				$paymentNode->appendChild($xml->createElement('ConsigneeBilled'));
+			}
+		} else if ($shipment->ItemizedPaymentInformation) {
+			//$paymentNode = $shipmentNode->appendChild($xml->createElement('ItemizedPaymentInformation'));
+		}
+
+		if ($shipment->GoodsNotInFreeCirculationIndicator) {
+			$shipmentNode->appendChild($xml->createElement('GoodsNotInFreeCirculationIndicator'));
+		}
+
+		if ($shipment->MovementReferenceNumber) {
+			$shipmentNode->appendChild($xml->createElement('MovementReferenceNumber', $shipment->MovementReferenceNumber));
+		}
+
+		$serviceNode = $shipmentNode->appendChild($xml->createElement('Service'));
+		$serviceNode->appendChild($xml->createElement('Code', $shipment->Service->Code));
+
+		if ($shipment->Service->Description) {
+			$serviceNode->appendChild($xml->createElement('Description', $shipment->Service->Description));
+		}
+
+		if ($shipment->InvoiceLineTotal) {
+			$node = $shipmentNode->appendChild($xml->createElement('InvoiceLineTotal'));
+
+			if ($shipment->InvoiceLineTotal->CurrencyCode) {
+				$node->appendChild($xml->createElement('CurrencyCode', $shipment->InvoiceLineTotal->CurrencyCode));
+			}
+
+			$node->appendChild($xml->createElement('MonetaryValue', $shipment->InvoiceLineTotal->MonetaryValue));
+		}
+
+		if ($shipment->NumOfPiecesInShipment) {
+			$shipmentNode->appendChild($xml->createElement('NumOfPiecesInShipment', $shipment->NumOfPiecesInShipment));
+		}
+
+		foreach ($shipment->Package as &$package) {
+			$node = $shipmentNode->appendChild($xml->createElement('Package'));
+
+			$ptNode = $node->appendChild($xml->createElement('PackagingType'));
+			$ptNode->appendChild($xml->createElement('Code', $package->PackagingType->Code));
+
+			if ($package->PackagingType->Description) {
+				$ptNode->appendChild($xml->createElement('Description', $package->PackagingType->Description));
+			}
+
+			$pwNode = $node->appendChild($xml->createElement('PackageWeight'));
+			$umNode = $pwNode->appendChild($xml->createElement('UnitOfMeasurement'));
+
+			if ($package->PackageWeight->UnitOfMeasurement->Code) {
+				$umNode->appendChild($xml->createElement('Code', $package->PackageWeight->UnitOfMeasurement->Code));
+			}
+
+			if ($package->PackageWeight->UnitOfMeasurement->Description) {
+				$umNode->appendChild($xml->createElement('Description', $package->PackageWeight->UnitOfMeasurement->Description));
+			}
+
+			$pwNode->appendChild($xml->createElement('Weight', $package->PackageWeight->Weight));
+
+			if ($package->LargePackageIndicator) {
+				$node->appendChild($xml->createElement('LargePackageIndicator'));
+			}
+
+			if ($package->ReferenceNumber) {
+				$refNode = $node->appendChild($xml->createElement('ReferenceNumber'));
+
+				if ($package->ReferenceNumber->BarCodeIndicator) {
+					$refNode->appendChild($xml->createElement('BarCodeIndicator', $package->ReferenceNumber->BarCodeIndicator));
+				}
+
+				$refNode->appendChild($xml->createElement('Code', $package->ReferenceNumber->Code));
+				$refNode->appendChild($xml->createElement('Value', $package->ReferenceNumber->Value));
+			}
+
+			if ($package->AdditionalHandling) {
+				$refNode->appendChild($xml->createElement('AdditionalHandling'));
+			}
+		}
+
+		if ($labelSpecificationOpts) {
+			$labelSpec = $container->appendChild($xml->createElement('LabelSpecification'));
+
+			$node = $labelSpec->appendChild($xml->createElement('LabelPrintMethod'));
+			$node->appendChild($xml->createElement('Code', $labelSpecOpts->LabelPrintMethod->Code));
+
+			if ($labelSpecOpts->LabelPrintMethod->Description) {
+				$node->appendChild($xml->createElement('Description', $labelSpecOpts->LabelPrintMethod->Description));
+			}
+
+			if ($labelSpecOpts->HTTPUserAgent) {
+				$labelSpec->appendChild($xml->createElement('HTTPUserAgent', $labelSpecOpts->HTTPUserAgent));
+			}
+
+			if ($labelSpecOpts->LabelStockSize) {
+				$stock = $labelSpec->appendChild($xml->createElement('LabelStockSize'));
+
+				$stock->appendChild($xml->createElement('Height', $labelSpecOpts->LabelStockSize->Height));
+				$stock->appendChild($xml->createElement('Width', $labelSpecOpts->LabelStockSize->Width));
+			}
+
+			$node = $labelSpec->appendChild($xml->createElement('LabelImageFormat'));
+			$node->appendChild($xml->createElement('Code', $labelSpecificationOpts->ImageFormat->Code));
+
+			if ($labelSpecOpts->ImageFormat->Description) {
+				$node->appendChild($xml->createElement('Description', $labelSpecOpts->ImageFormat->Description));
+			}
+
+			if ($labelSpecOpts->Instruction) {
+				$node = $labelSpec->appendChild($xml->createElement('Instruction'));
+				$node->appendChild($xml->createElement('Code', $labelSpecificationOpts->Instruction->Code));
+
+				if ($labelSpecOpts->Instruction->Description) {
+					$node->appendChild($xml->createElement('Description', $labelSpecOpts->Instruction->Description));
+				}
+			}
+		}
+
+		if ($receiptSpecOpts) {
+			$receiptSpec = $container->appendChild($xml->createElement('ReceiptSpecification'));
+			$node = $receiptSpec->appendChild($xml->createElement('ImageFormat'));
+			$node->appendChild($xml->createElement('Code', $receiptSpecOpts->ImageFormat->Code));
+
+			if ($receiptSpecOpts->ImageFormat->Description) {
+				$node->appendChild($xml->createElement('Description', $receiptSpecOpts->ImageFormat->Description));
+			}
+		}
 
 		return $xml->saveXML();
 	}
@@ -99,7 +415,7 @@ class Shipping extends UPS {
 		$request->appendChild($node);
 
 		$request->appendChild($xml->createElement('RequestAction', 'ShipAccept'));
-		$request->appendChild($xml->createElement('ShipmentDigest', $shipmentDigest));
+		$container->appendChild($xml->createElement('ShipmentDigest', $shipmentDigest));
 
 		return $xml->saveXML();
 	}
@@ -266,5 +582,44 @@ class Shipping extends UPS {
 	 */
 	private function formatResponse(SimpleXMLElement $response) {
 		return $this->convertXmlObject($response);
+	}
+
+	/**
+	 * Generates a standard <Address> node for requests
+	 *
+	 * @param  stdClass Address data structure
+	 * @return SimpleXMLElement
+	 */
+	private function compileAddressNode(&$address) {
+		$xml = new DOMDocument;
+		$xml->formatOutput = true;
+
+		$node = $xml->appendChild($xml->createElement('Address'));
+
+		$node->appendChild($xml->createElement('AddressLine1', $address->AddressLine1));
+
+		if ($address->AddressLine2) {
+			$node->appendChild($xml->createElement('AddressLine2', $address->AddressLine2));
+		}
+
+		if ($address->AddressLine3) {
+			$node->appendChild($xml->createElement('AddressLine3', $address->AddressLine3));
+		}
+
+		$node->appendChild($xml->createElement('City', $address->City));
+
+		if ($address->StateProvinceCode) {
+			$node->appendChild($xml->createElement('StateProvinceCode', $address->StateProvinceCode));
+		}
+
+		if ($address->PostalCode) {
+			$node->appendChild($xml->createElement('PostalCode', $address->PostalCode));
+		}
+
+		if ($address->CountryCode) {
+			$node->appendChild($xml->createElement('CountryCode', $address->CountryCode));
+		}
+
+		return $node->cloneNode(true);
 	}
 }
