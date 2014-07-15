983175c0f0eb264c177832aaeca26e74a44265d8
sebastien <sebastien@unclejeans.com>
2014-07-15 16:46:44 +0200

    Fix S_AIR_2DAYAM const

--- src/Ups/Entity/Service.php
+++ src/Ups/Entity/Service.php
@@ -11,7 +11,7 @@ class Service implements NodeInterface
     const S_AIR_1DAYEARLYAM = '14';
     const S_AIR_1DAY = '01';
     const S_AIR_1DAYSAVER = '13';
-    const S_AIR_2DAYAM = '29';
+    const S_AIR_2DAYAM = '59';
     const S_AIR_2DAY = '02';
     const S_3DAYSELECT = '12';
     const S_GROUND = '03';
