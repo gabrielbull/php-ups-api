d4609966a7421f83fceafae13729de0ee3ccd610
Martin Bartusek <mbartusek@section.cz>
2014-08-26 15:32:10 +0200

    unify shopRates() and getRates() in Rate class

--- src/Ups/Rate.php
+++ src/Ups/Rate.php
@@ -43,6 +43,13 @@ class Rate extends Ups
      */
     public function shopRates($rateRequest)
     {
+
+        if ($rateRequest instanceof Shipment) {
+            $shipment = $rateRequest;
+            $rateRequest = new RateRequest();
+            $rateRequest->setShipment($shipment);
+        }
+
         $this->requestOption = "Shop";
 
         return $this->sendRequest($rateRequest);
