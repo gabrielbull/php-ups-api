16e599dd6d93f762402fa68d9204fa2780e5ebf6
hm122 <hm12git@mail.at99.at>
2015-11-15 23:23:22 +0100

    Adds support for cash on delivery for shipments

--- src/Entity/CODAmount.php
+++ src/Entity/CODAmount.php
@@ -2,7 +2,11 @@
 
 namespace Ups\Entity;
 
-class CODAmount
+use DOMDocument;
+use DOMElement;
+use Ups\NodeInterface;
+
+class CODAmount implements NodeInterface
 {
     public $CurrencyCode;
     public $MonetaryValue;
@@ -17,5 +21,61 @@ class CODAmount
                 $this->MonetaryValue = $response->MonetaryValue;
             }
         }
+    }
+
+    /**
+     * @param null|DOMDocument $document
+     *
+     * @return DOMElement
+     */
+    public function toNode(DOMDocument $document = null)
+    {
+        if (null === $document) {
+            $document = new DOMDocument();
+        }
+        $node = $document->createElement('CODAmount');
+        if ($this->CurrencyCode) {
+            $node->appendChild($document->createElement('CurrencyCode', $this->CurrencyCode));
+        }
+        if ($this->MonetaryValue) {
+            $node->appendChild($document->createElement('MonetaryValue', $this->MonetaryValue));
+        }
+        return $node;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrencyCode()
+    {
+        return $this->CODCode;
+    }
+
+    /**
+     * @param mixed $CurrencyCode
+     * @return CODAmount
+     */
+    public function setCurrencyCode($CurrencyCode)
+    {
+        $this->CurrencyCode = $CurrencyCode;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonetaryValue()
+    {
+        return $this->MonetaryValue;
+    }
+
+    /**
+     * @param mixed $MonetaryValue
+     * @return CODAmount
+     */
+    public function setMonetaryValue($MonetaryValue)
+    {
+        $this->MonetaryValue = $MonetaryValue;
+        return $this;
     }
 }
