21d6b85e7b321e5991f02395fff5023bb4286aae
Pierre Tondereau <ptondereau@users.noreply.github.com>
2020-09-14 20:22:50 +0000

    Apply fixes from StyleCI

--- src/AddressValidation.php
+++ src/AddressValidation.php
@@ -192,8 +192,10 @@ class AddressValidation extends Ups
                 $addressNode->appendChild($xml->createElement('AddressLine', $this->address->getAddressLine3()));
             }
             if ($this->address->getStateProvinceCode()) {
-                $addressNode->appendChild($xml->createElement('PoliticalDivision1',
-                    $this->address->getStateProvinceCode()));
+                $addressNode->appendChild($xml->createElement(
+                    'PoliticalDivision1',
+                    $this->address->getStateProvinceCode()
+                ));
             }
             if ($this->address->getCity()) {
                 $addressNode->appendChild($xml->createElement('PoliticalDivision2', $this->address->getCity()));
